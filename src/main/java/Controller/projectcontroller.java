4a102a10b1f647b9e6d0318ec70f595ff514b91e
Zheer09 <bestzheer@gmail.com>
2022-01-11 08:42:56 +0300

    fix add project

--- src/main/java/Controller/projectcontroller.java
+++ src/main/java/Controller/projectcontroller.java
@@ -4,21 +4,25 @@
  */
 package Controller;
 
+import Bean.Profile;
 import static com.sun.org.apache.xalan.internal.lib.ExsltDatetime.date;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -44,33 +48,46 @@ public class projectcontroller extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
-     
+  HttpSession session=request.getSession(); 
+     Profile user =(Profile)session.getAttribute("profile");
         String title = request.getParameter("Title");
         String budget = request.getParameter("Budget");
-        String date1 = request.getParameter("Date");
+        String date1 = request.getParameter("date");
         String type = request.getParameter("type");
-        String discription = request.getParameter("discription");
+        String description = request.getParameter("discription");
 
-          Date date;
-        try {
-            date = new SimpleDateFormat("yyyy-MM-dd").parse(date1);
-        } catch (ParseException ex) {
-            Logger.getLogger(projectcontroller.class.getName()).log(Level.SEVERE, null, ex);
-        }
+            //session.setAttribute("profile", user);
        
         try {
         Connection con = DBconnection.createConnection();
-        String query = "insert into projects(projectTitle,projectType,projectDescription,projectBudget,projectDate) VALUE (?,?,?,?,?)";
-        PreparedStatement pres;
-        pres = null;
-        pres = con.prepareStatement(query);
-        pres.setDate(index, new java.sql.Date(date.getTime()));
+        DateFormat formatter ;
+        formatter = new SimpleDateFormat("dd-MM-yyyy");
+        Date date=formatter.parse(request.getParameter("date"));
         
+        String query = "insert into projects(projectTitle,projectType,projectDescription,projectBudget,projectDate, userID ,projectStatus) VALUE (?,?,?,?,?,?,?)";
+        PreparedStatement pres;
+        pres = con.prepareStatement(query);
+        pres.setString(1, title);
+        pres.setString(2, type);
+        pres.setString(3, description);
+        pres.setString(4, budget);
+        pres.setString(5, date1);
+        pres.setInt(6, user.getUserID());
+        pres.setString(7, "pending");
+
+        int i =  pres.executeUpdate();
+
+            if (i!=0){
+            RequestDispatcher dis = request.getRequestDispatcher("ClientView.jsp");
+            dis.forward(request, response);
+            }
+
         } catch(SQLException e)
          {
             e.printStackTrace();
-         } 
+         } catch (ParseException ex) { 
+            Logger.getLogger(projectcontroller.class.getName()).log(Level.SEVERE, null, ex);
+        } 
     }
 
     /**
